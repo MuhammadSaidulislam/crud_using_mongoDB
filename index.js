41fb50eebfecf034269beff0ee7a449634d12037
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-06-30 16:45:51 +0600

    mongodb data recive from ui

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var express = require("express");
-var app = express();
-const password = "tnyn9EdEQgj1JqB2";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// mongodb setting
 const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://Saidul:tnyn9EdEQgj1JqB2@cluster0.npcff.mongodb.net/organicdb?retryWrites=true&w=majority";
@@ -9,22 +14,34 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// data get method
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// mongodb client connected
 client.connect((err) => {
   const collection = client.db("organicdb").collection("product");
   // perform actions on the collection object
-  const products = { name: "water",price:15,quantity:50 };
-  collection.insertOne(products).then((res) => {
-    console.log("product add");
+
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    collection.insertOne(product)
+    .then(result => {
+      console.log("data send");
+      res.send("Success");
+    });
   });
+
   console.log("Database update");
-  
 });
 
-const user = ["one", "userOne", "userTwo", "userthree", "userFour"];
-app.get("/user/:id", (req, res) => {
-  const id = req.params.id;
-  const name = user[id];
-  res.send({ name, id });
-});
+// const user = ["one", "userOne", "userTwo", "userthree", "userFour"];
+// app.get("/user/:id", (req, res) => {
+//   const id = req.params.id;
+//   const name = user[id];
+//   res.send({ name, id });
+// });
 
 app.listen(4200, () => console.log("listening port 4200"));
