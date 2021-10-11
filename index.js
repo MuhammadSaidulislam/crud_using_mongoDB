ff57e73cc17d2bf28c5b7bf22da8aeaebedeb6b6
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-10-11 21:12:38 +0600

    path set

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+// app.get('/', function (req, res) {
+//     const array={
+//         product: 'saidul',
+//         price: '10'
+//     }
+//     res.send(array)
+// });
+
+const user = ['', 'userOne','userTwo','userthree','userFour']
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const name = user[id];
+    res.send({name,id});
+});
+
+
+app.listen(3000, () => console.log('listening port 3000'));
