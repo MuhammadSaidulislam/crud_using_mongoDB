4fff2e4d553d529af08ddb2b6b2742d812645c7f
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-12-04 14:09:32 +0600

    mongo sewt

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
 var express = require('express')
 var app = express()
+const password="Saidul";
 
-// app.get('/', function (req, res) {
-//     const array={
-//         product: 'saidul',
-//         price: '10'
-//     }
-//     res.send(array)
-// });
-
-const user = ['', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
-});
-const user = ['', 'userOne','userTwo','userthree','userFour']
+const user = ['one', 'userOne','userTwo','userthree','userFour']
 app.get('/user/:id', (req,res)=>{
     const id = req.params.id;
     const name = user[id];
     res.send({name,id});
 });
 
-const user = ['', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
-});
-const user = ['', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
-});
-
-app.listen(3000, () => console.log('listening port 3000'));
+app.listen(4200, () => console.log('listening port 4200'));
