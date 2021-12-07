c410e1a46d331b049b6b5e8687275a68ada95814
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-12-07 14:41:17 +0600

    mongodb data send succesfully

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express')
-var app = express()
-const password="Saidul";
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://Saidul:Saidul@cluster0.95kc4.mongodb.net/organicdb?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
+var express = require("express");
+var app = express();
+const password = "tnyn9EdEQgj1JqB2";
+
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://Saidul:tnyn9EdEQgj1JqB2@cluster0.npcff.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("organicdb").collection("product");
   // perform actions on the collection object
-  client.close();
+  const products = { name: "water",price:15,quantity:50 };
+  collection.insertOne(products).then((res) => {
+    console.log("product add");
+  });
+  console.log("Database update");
+  
 });
 
-const user = ['one', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
+const user = ["one", "userOne", "userTwo", "userthree", "userFour"];
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  const name = user[id];
+  res.send({ name, id });
 });
 
-app.listen(4200, () => console.log('listening port 4200'));
+app.listen(4200, () => console.log("listening port 4200"));
